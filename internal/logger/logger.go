48bdd40645219bcda51a297244beb1c6a8e64601
adachng <11900409+adachng@users.noreply.github.com>
2026-03-26 20:19:49 +0800

    feat(internal): add `logger` package

---
+++ internal/logger/logger.go
@@ -0,0 +1,24 @@
+package logger
+
+import (
+	"log"         // Default(), log.Logger
+	"sync/atomic" // atomic.Pointer
+)
+
+var loggerSingleton atomic.Pointer[log.Logger]
+
+func init() { // https://go.dev/doc/effective_go#init
+	loggerSingleton.Store(log.Default())
+}
+
+func GetInstance() *log.Logger {
+	return loggerSingleton.Load()
+}
+
+func SetInstance(instance *log.Logger) {
+	if instance == nil {
+		loggerSingleton.Store(log.Default())
+	} else {
+		loggerSingleton.Store(instance)
+	}
+}
