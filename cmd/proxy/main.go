52f5551f8f6a039bc4aa79e00962502121b6504e
adachng <11900409+adachng@users.noreply.github.com>
2026-03-28 09:18:20 +0800

    refactor(cmd/proxy): clean up

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -21,11 +21,6 @@ type config struct {
 	dstAddr    string // e.g. "192.168.0.0:1234"
 }
 
-type app struct {
-	c        config
-	listener net.Listener
-}
-
 func parseIntoConfig() config {
 	var ret config
 
@@ -48,19 +43,21 @@ func parseIntoConfig() config {
 }
 
 func validateConfig(c config) error {
+	var ret error
+
 	if c.listenPort <= 0 {
-		return errors.New("Invalid c.listenPort = [" + strconv.FormatUint(uint64(c.listenPort), 10) + "]")
+		ret = errors.New("Invalid c.listenPort = [" + strconv.FormatUint(uint64(c.listenPort), 10) + "]")
 	}
 
 	if len(c.dstAddr) <= 0 { // || !isValidIPv4(c.dstAddr) {
-		return errors.New("Invalid c.dstAddr = [" + c.dstAddr + "] with len [" + strconv.FormatInt(int64(len(c.dstAddr)), 10) + "]")
+		ret = errors.New("Invalid c.dstAddr = [" + c.dstAddr + "] with len [" + strconv.FormatInt(int64(len(c.dstAddr)), 10) + "]")
 	}
 
 	if len(c.srcAddr) <= 0 || !isValidIPv4(c.srcAddr) {
-		return errors.New("Invalid c.srcAddr = [" + c.srcAddr + "] with len [" + strconv.FormatInt(int64(len(c.srcAddr)), 10) + "]")
+		ret = errors.New("Invalid c.srcAddr = [" + c.srcAddr + "] with len [" + strconv.FormatInt(int64(len(c.srcAddr)), 10) + "]")
 	}
 
-	return nil
+	return ret
 }
 
 func isValidIPv4(s string) bool {
@@ -81,44 +78,39 @@ func isValidIPv4(s string) bool {
 }
 
 func main() {
-	var a app
-
 	// App config:
-	a.c = parseIntoConfig()
-
-	// Log CLI input:
-	{
-		currentLogger := logger.Get()
-
-		currentLogger.Printf("c.dstAddr = [%s]\n", a.c.dstAddr)
-		currentLogger.Printf("c.srcAddr = [%s]\n", a.c.srcAddr)
-		currentLogger.Printf("c.listenPort = [%v]\n", a.c.listenPort)
-	}
+	c := parseIntoConfig()
 
 	// Validate config:
 	{
-		err := validateConfig(a.c)
+		err := validateConfig(c)
 		if err != nil {
 			logger.Get().Fatal(err)
 		}
 	}
 
-	// Establish listener:
+	// Log CLI input:
 	{
-		listenAddr := ":" + strconv.FormatUint(uint64(a.c.listenPort), 10)
+		currentLogger := logger.Get()
 
-		logger.Get().Printf("Proxy listening at [%s]\n", listenAddr)
+		currentLogger.Printf("c.dstAddr = [%s]\n", c.dstAddr)
+		currentLogger.Printf("c.srcAddr = [%s]\n", c.srcAddr)
+		currentLogger.Printf("c.listenPort = [%v]\n", c.listenPort)
+	}
 
-		var err error
-		a.listener, err = net.Listen("tcp", listenAddr)
-		if err != nil {
-			log.Default().Fatal(err)
-		}
+	// Establish listener:
+	listenAddr := ":" + strconv.FormatUint(uint64(c.listenPort), 10)
+
+	logger.Get().Printf("Proxy listening at [%s]\n", listenAddr)
+
+	listener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Default().Fatal(err)
 	}
 
 	// Defer the listener.Close() to end of main():
 	defer func() {
-		err := a.listener.Close()
+		err := listener.Close()
 		if err != nil {
 			log.Default().Panic(err)
 		}
@@ -126,16 +118,16 @@ func main() {
 
 	// Main loop:
 	for {
-		conn, err := a.listener.Accept() // blocking function
+		conn, err := listener.Accept() // blocking function
 
 		if err != nil {
 			logger.Get().Printf("Accept() error: [%s]\n", err)
 			continue
 		}
 
-		if true { // conn.RemoteAddr().String() == a.c.srcAddr {
+		if true { // conn.RemoteAddr().String() == c.srcAddr {
 			logger.Get().Printf("Accepted source connection from [%s]\n", conn.RemoteAddr().String())
-			go handleConn(conn, a.c.dstAddr)
+			go handleConn(conn, c.dstAddr)
 		} else {
 			logger.Get().Printf("Invalid source connection accepted from [%s], closing connection\n", conn.RemoteAddr().String())
 			err := conn.Close()
@@ -153,7 +145,7 @@ func handleConn(srcConn net.Conn, dstAddr string) {
 	// UUID to represent source connection:
 	connId := uuid.New().String()
 	logger.Get().Printf(
-		"[%s]: source connection [%s] tied to UUID\n",
+		"[%s]: Source connection [%s] tied to UUID\n",
 		connId,
 		srcConn.RemoteAddr().String(),
 	)
@@ -162,7 +154,7 @@ func handleConn(srcConn net.Conn, dstAddr string) {
 	defer func() {
 		err := srcConn.Close()
 		logger.Get().Printf(
-			"[%s]: source connection [%s] closed\n",
+			"[%s]: Source connection [%s] closed\n",
 			connId,
 			srcConn.RemoteAddr().String(),
 		)
@@ -179,7 +171,7 @@ func handleConn(srcConn net.Conn, dstAddr string) {
 	}
 
 	logger.Get().Printf(
-		"[%s]: dialed to [%s] successfully\n",
+		"[%s]: Dialed to [%s] successfully\n",
 		connId,
 		dstConn.RemoteAddr().String(),
 	)
@@ -188,7 +180,7 @@ func handleConn(srcConn net.Conn, dstAddr string) {
 	defer func() {
 		err := dstConn.Close()
 		logger.Get().Printf(
-			"[%s]: destination connection [%s] closed\n",
+			"[%s]: Destination connection [%s] closed\n",
 			connId,
 			dstConn.RemoteAddr().String(),
 		)
@@ -220,39 +212,3 @@ func handleConn(srcConn net.Conn, dstAddr string) {
 
 	s.Wait()
 }
-
-// func handleSrcConn(srcConn net.Conn, dstAddr string) {
-// 	defer func() {
-// 		err := srcConn.Close()
-// 		if err != nil {
-// 			logger.Get().Panic(err)
-// 		}
-// 	}()
-
-// 	dstConn, err := net.Dial("tcp", dstAddr)
-// 	if err != nil {
-// 		logger.Get().Panic(err)
-// 	}
-
-// 	go handleDestConn(dstConn, srcConn)
-
-// 	buf := make([]byte, 1024)
-
-// 	for {
-// 		nread, err := srcConn.Read(buf)
-// 		// _ = srcConn.Read()
-// 	}
-// }
-
-// func handleDestConn(dstConn net.Conn, srcConn net.Conn) {
-// 	defer func() {
-// 		err := dstConn.Close()
-// 		if err != nil {
-// 			logger.Get().Panic(err)
-// 		}
-// 	}()
-
-// 	for {
-// 		dstConn.Read()
-// 	}
-// }
